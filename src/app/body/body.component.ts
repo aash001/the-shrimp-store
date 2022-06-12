d5119284f04190b7a8f8ecf673c445d87a65c373
aash001 <ampinarioash@users.noreply.github.com>
2022-06-12 00:57:56 -0400

    Add routing to home page

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.scss']
+  styleUrls: ['./body.component.scss'],
 })
-export class BodyComponent implements OnInit {
+export class BodyComponent {
+  @Input() collapsed = false;
+  @Input() screenWidth = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getBodyClass(): string {
+    let styleClass = '';
+    if (this.collapsed && this.screenWidth > 768) {
+      styleClass = 'body-trimmed';
+    } else if (
+      this.collapsed &&
+      this.screenWidth <= 768 &&
+      this.screenWidth > 0
+    ) {
+      styleClass = 'body-md-screen';
+    }
+    return styleClass;
   }
-
 }
